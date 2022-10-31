f8e28101a778fdd027fe404882bdbdb8c13ad1ac
Pedro Augusto <p.augusto_23@outlook.com>
2022-10-31 17:39:13 -0300

    Correcao

--- exercicio-01/exercicio01.c
+++ exercicio-01/exercicio01.c
@@ -5,26 +5,26 @@ int main()
 {
   float qtdHambuguer, qtdCheese, qtdBatata, qtdRefri, qtdMilki = 0;
   float valor[5] = {3.50, 4.50, 5.00, 1.50, 2.80};
-  float qtd_item[5];
+  float qtdItem[5];
   float result = 0;
 
   printf("\t\t\tCardapio Mona's Burguer: ");
 
   printf("\nInsira a quantidade de hamburguer que deseja: ");
-  scanf("%f", &qtd_item[0]);
-  result = valor[0] * qtd_item[0];
+  scanf("%f", &qtdItem[0]);
+  result = valor[0] * qtdItem[0];
   printf("\nInsira a quantidade de CheeseBurguer que deseja: ");
-  scanf("%f", &qtd_item[1]);
-  result = (valor[1] * qtd_item[1]) + result;
+  scanf("%f", &qtdItem[1]);
+  result = (valor[1] * qtdItem[1]) + result;
   printf("\nInsira a quantidade de Batata Frita que deseja: ");
-  scanf("%f", &qtd_item[2]);
-  result = (valor[2] * qtd_item[2]) + result;
+  scanf("%f", &qtdItem[2]);
+  result = (valor[2] * qtdItem[2]) + result;
   printf("\nInsira a quantidade de Refrigerante que deseja: ");
-  scanf("%f", &qtd_item[3]);
-  result = (valor[3] * qtd_item[3]) + result;
+  scanf("%f", &qtdItem[3]);
+  result = (valor[3] * qtdItem[3]) + result;
   printf("\nInsira a quantidade de Milk-Shake que deseja: ");
-  scanf("%f", &qtd_item[4]);
-  result = (valor[4] * qtd_item[4]) + result;
+  scanf("%f", &qtdItem[4]);
+  result = (valor[4] * qtdItem[4]) + result;
 
   printf("\n\nO valor total da sua compra eh de: R$%.2f\n", result);
 
