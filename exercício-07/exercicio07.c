c0519c8d6852cc897a393d140eb71bce2badf22b
Pedro Augusto <p.augusto_23@outlook.com>
2022-10-31 18:31:22 -0300

    exercicio 07

--- exercício-07/exercicio07.c
+++ exercício-07/exercicio07.c
@@ -3,6 +3,21 @@
 
 int main()
 {
+  float rawSalary = 3000, salary, comission, sellingPercent, qtdSelling, totalSelling;
+
+  printf("\t\tCompanhia de carros usados João Honesto");
+
+  printf("Informe o a quantidade de vendas: ");
+  scanf("%f", &qtdSelling);
+  printf("Informe o total de vendas: ");
+  scanf("%f", &totalSelling);
+
+  comission = qtdSelling * 500;
+  sellingPercent = totalSelling * 0.05;
+
+  salary = rawSalary + comission + sellingPercent;
+
+  printf("O salario a receber sera de: %.2f\n", salary);
 
   system("pause");
 }
